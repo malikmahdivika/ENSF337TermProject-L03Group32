e1470b8d273dcb1265d59cb6f99d26de4d9833b3
malikmahdivika <144976344+malikmahdivika@users.noreply.github.com>
2023-11-23 22:17:33 -0700

    all "required" functions are defined, some are commented out since I'm not sure if they'd be useful

--- FlightClass.h
+++ FlightClass.h
@@ -14,9 +14,24 @@ class flight {
         //other data members to be added on need basis
 	
 	public:
-		flight(string ID, int row, int seats_per_row);
+        //ctors,destructors
+        flight();
+		flight(string ID, int row, int seatsperrow, vector <string> passlist, vector <vector<string>> map);
+        ~flight();
+        flight(const flight& source);
+
+        //getters
 		string get_flightID();
 		int get_seatrow();
+        vector get_passenger_list();
+        vector get_map();
+
+        //setters
+        void set_flightID(string input);
+		//void set_seatrow(); not sure if the following setters are useful...
+        //void set_passenger_list();
+        //void set_map();
+
         //other functions to be implemented on need basis
         
 }
