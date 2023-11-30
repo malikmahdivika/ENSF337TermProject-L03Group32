a418728b8292ba14c0fcd8f20920d0d6a6137af8
malikmahdivika <144976344+malikmahdivika@users.noreply.github.com>
2023-11-30 01:19:01 -0700

    more qol updates to variable naming etc.

--- FlightClass.cpp
+++ FlightClass.cpp
@@ -3,37 +3,26 @@
 
 //getters
 string flight::get_flightID() {return flightID;}
-int flight::get_seatrow() {return seatrow;}
-int flight::get_seats_per_row() {return seats_per_row;}
+int flight::get_rows() {return rows;}
+int flight::get_cols() {return cols;}
 vector<string> flight::get_passenger_list() {return pList;}
 seatmap flight::get_map() {return seats;}
 
 //ctors, dtors
 flight::flight() {
     flightID = 'null';
-    seatrow = 0;
-    seats_per_row = 0;
-}
-flight::flight(string ID, int row, int seats_per_row, vector <string> pList, seatmap seats) {
-    ifstream in_stream('flight_info.txt'); //change to whatever file it is 
-    while(in_stream.fail()) {
-        string a;
-        cout << "file not found; please enter proper filename in \"filename.txt\" format." << endl;
-        cin >> a;
-        in_stream.open(a);
-    }
-    while (!in_stream.eof()) {
-        //read file
-    }
-
-
-    in_stream.close();
-
+    rows = 0;
+    cols = 0;
 }
 ~flight();
 flight(const flight& source);
 
 //test block
 int main() {
-    
+    flight plane;
+    int ID = plane.get_flightID();
+    int rows = plane.get_rows();
+    int cols = plane.get_cols();
+
+    cout << ID << rows << cols;
 }
--- FlightClass.h
+++ FlightClass.h
@@ -11,8 +11,8 @@ typedef vector<vector<string>> seatmap;
 class flight {
 	private:
 		string flightID;
-		int seatrow; //rows are numbers, columns letters
-		int seats_per_row;
+		int rows; //rows are numbers, columns letters
+		int cols;
 		vector <string> pList;
 		seatmap seats;
         //other data members to be added on need basis
@@ -20,20 +20,21 @@ class flight {
 	public:
         //ctors,destructors
         flight();
-		flight(string ID, int row, int seats_per_row, vector <string> pList, seatmap seats);
+		flight(string ID, int rows, int cols, vector <string> pList, seatmap seats);
         ~flight();
         flight(const flight& source);
 
         //getters
 		string get_flightID();
-		int get_seatrow();
-        int get_seats_per_row();
+		int get_rows();
+        int get_cols();
         vector get_passenger_list();
         vector get_map();
 
         //setters
         void set_flightID(string input);
-		void set_seatrow(); //not sure if the following setters are useful...
+		void set_rows(); //not sure if the following setters are useful...
+        void set_cols();
         void set_passenger_list();
         void set_map();
 
